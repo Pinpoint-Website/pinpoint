0d197f7d8477288ceba655f98906d8fedfed6eb7
Devin Schutz <schutzdevin@gmail.com>
2025-09-17 20:18:57 -0600

    Got it so when a user signs up they get redirected to the login page with a message that the login was successful.

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,21 @@
+"use client"
+
 import { LoginForm } from "@/components/forms/login-form";
+import { useSearchParams } from "next/navigation";
 
 export default function Page() {
+  // get the search parameters
+  const searchParams = useSearchParams();
+  const showSuccess = searchParams.get("success") === "1";
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
+      <div className="flex flex-col items-center w-full max-w-md">
+        {showSuccess && (
+          <div className="mb-4 text-green-600 w-full text-center">
+            Your account was successfully created! Please log in.
+          </div>
+        )}
         <LoginForm />
       </div>
     </div>
